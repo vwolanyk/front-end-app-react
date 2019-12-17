9cea9980266f76ce71851cd9dd1901fc76d10ad9
Vlad Wolanyk <vladwolanyk@gmail.com>
2019-12-17 12:03:43 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { text: '' }
+
+  componentDidMount() {
+    window.fetch('http://localhost:3001/notes/1').then(data => {
+      data.json().then(res => {
+        this.setState({ text: res.text })
+      })
+    })
+  }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value })
+  }
+
+  render() {
+    return (
+      <textarea
+        value={this.state.text}
+        onChange={this.handleChange}
+      />
+    )
+  }
 }
 
-export default App;
+export default App
